55d405941b16b528845a0b4a2486f61fece8b491
hstcris <143611263+hstcris@users.noreply.github.com>
2024-03-02 15:56:48 -0300

    Create exercicio12.java

---
+++ linguagem-programaçao/tarefa2/exercicio12.java
@@ -0,0 +1,11 @@
+class ValorMaior {
+    public static void main(String[] args) {
+        int valor1= 8;
+        int valor2 = 6;
+        if (valor1 > valor2) {
+            System.out.println( valor1 + " é maior que " + valor2);
+        } else if (valor2 > valor1) {
+            System.out.println( valor2 + " é maior que " + valor1);
+      
+    }
+}
